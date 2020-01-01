558038d2d6efdfb5462405e8a933e5b0f1f46085
俊华 蔡 <1032031912@qq.com>
2020-01-01 10:23:18 +0800

    验证登陆输入框的账号密码不嫩为空

--- StudentManager/FrmUserLogin.cs
+++ StudentManager/FrmUserLogin.cs
@@ -21,7 +21,25 @@ namespace StudentManager
         //登录
         private void btnLogin_Click(object sender, EventArgs e)
         {
-
+            #region 验证数据是否符合要求
+            //账号不能为空
+            if (this.txtLoginId.Text.Trim().Length==0)
+            {
+                //提示
+                MessageBox.Show("请输入登陆账号!", "提示信息");
+                this.txtLoginId.Focus();
+                return;
+            }
+            //密码不能为空
+            if (this.txtLoginPwd.Text.Trim().Length == 0)
+            {
+                //提示
+                MessageBox.Show("请输入登陆密码!", "提示信息");
+                this.txtLoginPwd.Focus();
+                return;
+            }
+            //密码不能为空
+            #region
         }
         //关闭
         private void btnClose_Click(object sender, EventArgs e)
