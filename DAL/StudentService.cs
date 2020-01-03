37b6816482ca3ece509dc5321c66a31ef82881a9
俊华 蔡 <1032031912@qq.com>
2020-01-03 10:21:26 +0800

    完成学员信息管理-按班级查询/按学号精确查询

--- DAL/StudentService.cs
+++ DAL/StudentService.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using DAL.Helper;
 using Models;
-
 namespace DAL
 {
 
@@ -64,7 +64,67 @@ namespace DAL
             //【3】发起请求
             return Convert.ToInt32(SQLHelper.GetSingleResult(sql));
         }
-
-
+        //根据班级查询信息
+        public List<Student> GetStudentsByClass(string ClassName)
+        {
+            //构建SQL语句
+            string sql = "select StudentId,StudentName,Gender,StudentIdNo,Birthday,PhoneNumber,ClassName from Students";
+            sql += " inner join StudentClass on Students.ClassId=StudentClass.ClassId";
+            sql += " where ClassName='{0}'";
+            //拼接SQL语句
+            sql = string.Format(sql, ClassName);
+            //调用执行
+            SqlDataReader objReader = SQLHelper.GetReader(sql);
+            //循环读取返回查询到的结果
+            List<Student> list = new List<Student>();
+            //
+            while (objReader.Read())
+            {
+                list.Add(new Student()
+                {
+                    StudentId = Convert.ToInt32(objReader["StudentId"]),
+                    StudentName = objReader["StudentName"].ToString(),
+                    Gender = objReader["Gender"].ToString(),
+                    PhoneNumber = objReader["PhoneNumber"].ToString(),
+                    Birthday = Convert.ToDateTime(objReader["Birthday"]),
+                    StudentIdNo = objReader["StudentIdNo"].ToString(),
+                    ClassName = objReader["ClassName"].ToString()
+                });
+            }
+            objReader.Close();
+            return list;
+        }
+        //根据学号查询信息
+        public Student GetStudentById(string studentId)
+        {
+            //构建SQL语句
+            string sql = "select StudentId,StudentName,Gender,Birthday,ClassName,";
+            sql += "StudentIdNo,PhoneNumber,StudentAddress,CardNo,StuImage from Students";
+            sql += " inner join StudentClass on Students.ClassId=StudentClass.ClassId ";
+            sql += " where StudentId=" + studentId;
+            //开始查询
+            //调用执行
+            SqlDataReader objReader = SQLHelper.GetReader(sql);
+            //接收返回值
+            Student objstudent = null;
+            if (objReader.Read())
+            {
+                objstudent = new Student()
+                {
+                    StudentId = Convert.ToInt32(objReader["StudentId"]),
+                    StudentName = objReader["StudentName"].ToString(),
+                    Gender = objReader["Gender"].ToString(),
+                    Birthday = Convert.ToDateTime(objReader["Birthday"]),
+                    ClassName = objReader["ClassName"].ToString(),
+                    CardNo = objReader["CardNo"].ToString(),
+                    StudentIdNo = objReader["StudentIdNo"].ToString(),
+                    PhoneNumber = objReader["PhoneNumber"].ToString(),
+                    StudentAddress = objReader["StudentAddress"].ToString(),
+                    StuImage = objReader["StuImage"] == null ? "" : objReader["StuImage"].ToString()
+                };
+            }
+            objReader.Close();
+            return objstudent;
+        }
     }
 }
--- StudentManager/FrmStudentManage.cs
+++ StudentManager/FrmStudentManage.cs
@@ -7,26 +7,75 @@ using System.Text;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
 
+using DAL;
+using Models;
+//using Common;
 
 namespace StudentManager
 {
     public partial class FrmStudentManage : Form
     {
+        //班级类
+        private StudentClassService objClassService = new StudentClassService();
+        //学生类
+        private StudentService objStudentService = new StudentService();
+        //定义一个List作为全局变量接受返回值
+        List<Student> list = null;
+
 
         public FrmStudentManage()
         {
             InitializeComponent();
-        
+            //初始化班级下拉框
+            this.cboClass.DataSource = objClassService.GetAllClasses();
+            this.cboClass.DisplayMember = "ClassName";
+            this.cboClass.ValueMember = "ClassId";
         }
         //按照班级查询
         private void btnQuery_Click(object sender, EventArgs e)
         {
-        
+            #region 数据验证
+            //提示选中班级
+            if (this.cboClass.SelectedIndex==-1)
+            { 
+                MessageBox.Show("请选择班级!", "提示");
+                return;
+            }
+            #endregion
+            //不显示未封装属性
+            this.dgvStudentList.AutoGenerateColumns = false;
+
+            //根据班级执行查询操作
+            list = objStudentService.GetStudentsByClass(this.cboClass.Text);
+            //绑定显示
+            this.dgvStudentList.DataSource = list;
+            //调整显示样式
+            new Common.DataGridViewStyle().DgvStyle1(this.dgvStudentList);
+
         }
         //根据学号查询
         private void btnQueryById_Click(object sender, EventArgs e)
         {
-          
+            #region 数据验证
+            if (this.txtStudentId.Text.Trim().Length==0)
+            {
+                MessageBox.Show("请输入学号!", "提示信息");
+                this.txtStudentId.Focus();
+                return;
+            }
+            //验证此学号在数据库中是否存在
+            Student objStudent = objStudentService.GetStudentById(this.txtStudentId.Text.Trim());
+            if (objStudent==null)
+            {
+                MessageBox.Show("学员信息不存在!", "提示信息");
+                this.txtStudentId.Focus();
+            }
+            else
+            {
+                FrmStudentInfo objFrmStudentInfo = new FrmStudentInfo(objStudent);
+                objFrmStudentInfo.Show();
+            }
+            #endregion
         }
         private void txtStudentId_KeyDown(object sender, KeyEventArgs e)
         {
